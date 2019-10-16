f9ffd1627669bc59afa26a4c8a0b7aa799466bc0
Thibault Renaud <Thibault.Renaud@student.uliege.be>
2019-10-16 16:02:22 +0200

    Remove thread and chunk

--- analysis.py
+++ analysis.py
@@ -1,137 +1,50 @@
 import pandas as pd
 from matplotlib import pyplot as plt
-import numpy as np
-import multiprocessing as mp
-import pandas as pd
-
-CHUNKSIZE_TOM = 5000000  # processing 1 000 000 rows at a time
-CHUNKSIZE_THIB = 1000000  # processing 1 000 000 rows at a time
-
-def get_packet_size(df):
-    columns_to_drop = [ 'dir',
-        'nh',
-        'nhb',
-        'svln',
-        'dvln',
-        'ismc',
-        'odmc',
-        'idmc',
-        'osmc',
-        'mpls1',
-        'mpls2',
-        'mpls3',
-        'mpls4',
-        'mpls5',
-        'mpls6',
-        'mpls7',
-        'mpls8',
-        'mpls9',
-        'mpls10',
-        'cl',
-        'sl',
-        'al',
-        'ra',
-        'engine_type',
-        'exid']
-    df.drop(columns=columns_to_drop,inplace=True)
-    print(df.src_mask.describe())
-
-    df['packet_size'] = df['in_bytes']/df['in_packets']
-
-    return df['packet_size']
 
 
-def get_flow_dur(df):
-    return df['time_duration']
-
-
-def get_packets_flow(df):
-    return df['in_packets']
-
-
-def get_bytes_flow(df):
-    return df['in_bytes']
-
-
-def get_volume_src_port(df):
-    return df.groupby(['src_port'])[['in_bytes']].sum().reset_index().sort_values(by=['in_bytes'], ascending=False)[:10]
-
-
-def get_volume_dest_port(df):
-    return df.groupby(['dest_port'])[['in_bytes']].sum().reset_index().sort_values(by=['in_bytes'], ascending=False)[:10]
-
-
-def exec_multi_proc_f(df_reader, pool, function_par):
-    joblist = []
-    for df in df_reader:
-        joblist.append(pool.apply_async(function_par,[df]))
-        break
-
-    df_list = []
-    for f in joblist:
-        df_list.append(f.get(timeout=10))
-    return df_list
-
-
-def create_graph(df_reader, pool, function_par, cum, is_log):
-    df_list = exec_multi_proc_f(df_reader, pool, function_par)
-
-    complete_df = pd.concat(df_list)
-    print(complete_df[:10])
-
+def create_graph(df, cum, is_log):
     fig, ax = plt.subplots()
     ax2 = ax.twinx()
 
     n_bins = 100
-    n, bins, patches = ax.hist(complete_df, bins=n_bins, density=False)
-    n, bins, patches = ax2.hist(complete_df, density=True, cumulative=cum, histtype='step',
+    n, bins, patches = ax.hist(df, bins=n_bins, density=False)
+    n, bins, patches = ax2.hist(df, density=True, cumulative=cum, histtype='step',
                                 label='CDF', bins=n_bins, log=is_log, color='tab:orange')
 
     plt.show()
 
 
-def question1(df_reader, pool):
+def question1(filename, new_names):
     # QUESTION 1: CDF of packet size
-    create_graph(df_reader, pool, get_packet_size, 1, False)
+    df = pd.read_csv(filename, header=0, names=new_names, usecols=['in_bytes', 'in_packets'])
+    create_graph(df['in_bytes']/df['in_packets'], 1, False)
 
 
-def question2(df_reader, pool):
+def question2(filename, new_names):
     # QUESTION 2: CCDF of flow duration, linear axis
-    create_graph(df_reader, pool, get_flow_dur, -1, False)
+    df = pd.read_csv(filename, header=0, names=new_names, usecols=['time_duration', 'in_bytes', 'in_packets'])
+    create_graph(df['time_duration'], -1, False)
+    create_graph(df['time_duration'], -1, True)
 
     # QUESTION 2: CCDF of number of bytes in a flow, linear axis
-    create_graph(df_reader, pool, get_bytes_flow, -1, False)
+    create_graph(df['in_bytes'], -1, False)
+    create_graph(df['in_bytes'], -1, True)
 
     # QUESTION 2: CCDF of number of packets in a flow, linear axis
-    create_graph(df_reader, pool, get_packets_flow, -1, False)
-
-    # QUESTION 2: CCDF of flow duration, log axis
-    create_graph(df_reader, pool, get_flow_dur, -1, True)
-
-    # QUESTION 2: CCDF of number of bytes in a flow, log axis
-    create_graph(df_reader, pool, get_bytes_flow, -1, True)
-
-    # QUESTION 2: CCDF of number of packets in a flow, log axis
-    create_graph(df_reader, pool, get_packets_flow, -1, True)
+    create_graph(df['in_packets'], -1, False)
+    create_graph(df['in_packets'], -1, True)
 
 
-def question3(df_reader, pool):
-    df_list = exec_multi_proc_f(df_reader, pool, get_volume_src_port)
-    print(df_list)
+def question3(filename, new_names):
+    df = pd.read_csv(filename, header=0, names=new_names, usecols=['src_port', 'in_bytes'])
+    print(df.groupby(['src_port'])[['in_bytes']].sum().reset_index().sort_values(by=['in_bytes'], ascending=False)[:10])
 
-    df_list = exec_multi_proc_f(df_reader, pool, get_volume_dest_port)
-    print(df_list)
+    df = pd.read_csv(filename, header=0, names=new_names, usecols=['dest_port', 'in_bytes'])
+    print(df.groupby(['dest_port'])[['in_bytes']].sum().reset_index().sort_values(by=['in_bytes'], ascending=False)[:10])
 
 
-def question4(df_reader, pool):
-    joblist = []
-    for df in df_reader:
-        joblist.append(pool.apply_async(get_flow_dur,[df]))
-        break
-
-    df_list = []
-    for f in joblist:
-        df_list.append(f.get(timeout=10))
+def question4(filename, new_names):
+    return
 
     
 if __name__ == '__main__':
@@ -187,12 +100,7 @@ if __name__ == '__main__':
         'engine_type',
         'exid']
 
-    df_reader = pd.read_csv(filename, chunksize=CHUNKSIZE_THIB, header=0, names=new_names)
-    pool = mp.Pool(4) # use 4 processes
-
-    # question1(df_reader, pool)
-    # question2(df_reader, pool)
-    question3(df_reader, pool)
-    #question4(df_reader, pool)
-
-
+    #question1(filename, new_names)
+    question2(filename, new_names)
+    question3(filename, new_names)
+    # question4(filename, new_names)
